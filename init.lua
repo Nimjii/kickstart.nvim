0d23d904cfc92c4e9d14073515a923df40d3a45c
Lukas Spreitzer <l.spreitzer@limesoda.com>
2023-08-19 15:52:27 +0200

    Add autopairs plugin

--- init.lua
+++ init.lua
@@ -55,6 +55,20 @@ require('lazy').setup({
   },
 
   {
+    'windwp/nvim-autopairs',
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  },
+
+  {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
