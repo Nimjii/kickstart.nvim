16827195e59ee6c34496afa80c267b6912aca8fa
Lukas Spreitzer <l.spreitzer@limesoda.com>
2023-08-19 22:31:52 +0200

    Add bindings for git-blame plugin

--- init.lua
+++ init.lua
@@ -163,6 +163,22 @@ require('lazy').setup({
 
   {
     'f-person/git-blame.nvim',
+    dependencies = {
+      'folke/which-key.nvim',
+    },
+    opts = function (_, opts)
+      require('which-key').register({
+        G = {
+          name = ' Git blame',
+          o = { '<cmd>GitBlameOpenCommitURL<cr>', 'Open commit url' },
+          h = { '<cmd>GitBlameCopySHA<cr>', 'Copy commit SHA' },
+          u = { '<cmd>GitBlameCopyCommitURL<cr>', 'Copy commit url' },
+          f = { '<cmd>GitBlameOpenFileURL<cr>', 'Open file url' },
+        },
+      }, { prefix = '<leader>'})
+
+      return opts
+    end,
     event = 'VeryLazy',
   },
 
