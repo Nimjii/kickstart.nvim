bec1351723e98a8c275a8c437cb6b151bd9b1763
Lukas Spreitzer <l.spreitzer@limesoda.com>
2023-09-27 20:30:50 +0200

    Replace tabs with filename in tab line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -74,17 +74,12 @@ return {
       },
       lualine_z = {
         {
-          'tabs',
-          mode = 1,
-          use_mode_colors = true,
-          fmt = function (_, context)
-            local buflist = vim.fn.tabpagebuflist(context.tabnr)
-            local winnr = vim.fn.tabpagewinnr(context.tabnr)
-            local bufnr = buflist[winnr]
-            local mod = vim.fn.getbufvar(bufnr, '&mod')
-
-            return context.tabnr .. (mod == 1 and ' 󰏪' or '')
-          end
+          'filename',
+          newfile_status = true,
+          symbols = {
+            modified = '󰏪',
+            readonly = '',
+          },
         },
       },
     }
