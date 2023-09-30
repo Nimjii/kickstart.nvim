5f7eb1f4814e24f1e0210db7799d9b4213684e10
Lukas Spreitzer <l.spreitzer@limesoda.com>
2023-09-30 11:39:42 +0200

    Migrate indent-blankline to v3

--- init.lua
+++ init.lua
@@ -81,13 +81,15 @@ require('lazy').setup({
   },
 
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    main = 'ibl',
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = '┊',
+      },
+      scope = {
+        enabled = false,
+      },
     },
   },
 
